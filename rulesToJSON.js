f3207024d8331cf55b098588b411557c73f5bbb0
Mael <mael.thomas@data.gouv.fr>
2022-02-17 17:41:20 +0100

    Séparation des fichiers de règles en plusieurs :)

--- rulesToJSON.js
+++ rulesToJSON.js
@@ -1,9 +1,16 @@
 let yaml = require('yaml')
 let fs = require('fs')
-fs.readFile('./data/co2.yaml', 'utf8', (err, data) => {
-	if (err) console.error(err)
-	let rules = yaml.parse(data)
+let glob = require('glob')
 
+// this file is kindof a duplicate of RulesProvider (which serves for the local watched webpack environment) in ecolab-climat
+// if it grows more than 20 lines, it should be shared
+
+glob('data/**/*.yaml', (err, files) => {
+	const rules = files.reduce((memo, filename) => {
+		const data = fs.readFileSync('./' + filename, 'utf8')
+		const rules = yaml.parse(data)
+		return { ...memo, ...rules }
+	}, {})
 	fs.writeFile('./public/co2.json', JSON.stringify(rules), function (err) {
 		if (err) return console.error(err)
 		console.log('Les règles en JSON ont été écrites avec succès, bravo !')
