b9544056c9f90e723aadaffab9761e984eebdbd1
Emile Rolley <emile.rolley@tuta.io>
2023-03-22 15:33:36 +0100

    pkg: setup npm compiled model publication

--- rulesToJSON.js
+++ rulesToJSON.js
@@ -5,6 +5,8 @@ let glob = require('glob')
 // this file is kindof a duplicate of RulesProvider (which serves for the local watched webpack environment) in ecolab-climat
 // if it grows more than 20 lines, it should be shared
 
+const destinationPath = process.argv[2] ?? './public/co2.json'
+
 glob('data/**/*.yaml', (err, files) => {
 	const rules = files.reduce((memo, filename) => {
 		const data = fs.readFileSync('./' + filename, 'utf8')
@@ -21,7 +23,7 @@ glob('data/**/*.yaml', (err, files) => {
 				: rules
 		return { ...memo, ...prefixedRuleSet }
 	}, {})
-	fs.writeFile('./public/co2.json', JSON.stringify(rules), function (err) {
+	fs.writeFile(destinationPath, JSON.stringify(rules), function (err) {
 		if (err) return console.error(err)
 		console.log('Les règles en JSON ont été écrites avec succès, bravo !')
 	})
